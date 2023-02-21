bb36f975d3cd692fc038dab4445d78dc05f141be
kubi77 <ac512626@gmailcom>
2023-02-21 08:08:33 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,10 @@ void print_alphabet_x10(void)
 	int j;
 	int x;
 
-	for (x = 0; x <= 10; x++)
+	for (j = 0; j <= 9; j++)
 	{
-		for (j = 'a'; j <= 'z'; j++)
-		{
-		_putchar(j);
-		}
+		for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
 	_putchar('\n');
 	}
 }
